a63b207ac23aa165ae6ccee9caafb72af4125a0f
Riddho <riddhoriddho@gmail.com>
2024-02-20 20:19:55 -0400

    SignUp with Existing User Validation

--- src/Authentication.java
+++ src/Authentication.java
@@ -7,10 +7,8 @@ public class Authentication {
     //File-Path of file containing user authentication data
     private static final String user_data="Data/User/users.csv";
 
+    //New User Sign Up
     public static boolean SignUp(String userID,String password){
-
-        //if user exists , check
-
 
         //Storing the password in hashed format
         String hashedPassword=Authentication.hashPassword(password);
@@ -32,7 +30,7 @@ public class Authentication {
 
     }
 
-
+    //Hash new user password using MD-5 algorithm
     private static String hashPassword(String password){
 
         try {
@@ -52,6 +50,29 @@ public class Authentication {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static boolean userExists(String userID){
+
+        //Traversing through the users.csv file and checking if the entered UserId already exists
+        try(BufferedReader br=new BufferedReader( new FileReader(user_data))){
+
+            String line;
+            while((line = br.readLine())!=null){
+
+                String storedUserID=line.split(",")[0];
+
+                if(storedUserID.equals(userID)){
+                    //returning true if found a user with passed userID
+                    return true;
+                }
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        //If UserId does not exist in users.csv, returning false.
+        return false;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,35 @@ public class Main {
 
         System.out.println("1. Sign-Up\n2. Login\n3. Exit");
 
-        Scanner sc=new Scanner(System.in);
-        int choice=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        String choice = sc.next();
+        
+            switch (choice) {
+                case "1":
+                    System.out.println("Enter UserId");
+                    String UserId = sc.next();
+                    sc.nextLine();
+                    if (Authentication.userExists(UserId)) {
+                        System.out.println("User already exists");
+                        break;
+                    }
+                    System.out.println("Enter Password");
+                    String Password = sc.next();
 
-        switch(choice){
-            case 1:
-                System.out.println("Enter UserId");
-                String UserId=sc.next();
-                sc.nextLine();
-                System.out.println("Enter Password");
-                String Password= sc.next();
+                    boolean SignedUp = Authentication.SignUp(UserId, Password);
+                    if (SignedUp) {
+                        System.out.println("Sign-Up successful");
+                    } else {
+                        System.out.println("User already exists");
+                    }
+                    break;
+                case "3":
+                    System.exit(0);
 
-                boolean SignedUp=Authentication.SignUp(UserId,Password);
-                if(SignedUp){
-                    System.out.println("User Signed In");
-                }
-                else {
-                    System.out.println("User already exists");
-                }
-        }
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
 
 
         }
